1be8c212c2b4cbbdf5712865647336813e89f431
HongThuan <guest@localhost.localdomain>
2019-05-29 18:15:01 +0700

    add

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from sys import argv
+from re import findall
+from json import dumps
 
 
 def read_log_file(log_file_pathname):
@@ -13,13 +15,45 @@ def read_log_file(log_file_pathname):
 
 def parse_log_start_time(log_data):
     format_log_date = 'Log Started at %A, %B %d, %Y %X'
-    return datetime.strptime(log_data.partition('\n')[0], format_log_date)
+    tz = log_data.partition('g_timezone,')[-1].partition(')')[0]
+    obj_datetime = datetime.strptime(log_data.partition('\n')[0], format_log_date)
+    obj_datetime = obj_datetime.replace(tzinfo = timezone(timedelta(hours=int(tz))))
+    return obj_datetime
+
+
+def parse_log_time(start_time, time_string, tz):
+    format_log_time = '%Y-%m-%d %H:%M:%S'
+    time_string = start_time[:14] + time_string[1:-1]
+    obj_datetime = datetime.strptime(time_string, format_log_time)
+    obj_datetime = obj_datetime.replace(tzinfo = tz)
+    return obj_datetime
+
+
+def parse_match_mode_and_map(log_data):
+    temporary = log_data.partition('Loading level Levels/')[-1].partition(' -')[0]
+    temporary = temporary.split()
+    return (temporary[-1], temporary[0][:-1])
+
+
+def parse_frags(log_data):
+    obj_datetime = parse_log_start_time(log_data)
+    output = []
+    for line in log_data.split('\n'):
+        if 'killed' in line:
+            line = line.split()
+            new_datetime = parse_log_time(str(obj_datetime), line[0], obj_datetime.tzinfo)
+            if len(line) == 7:
+                output.append((new_datetime, line[2], line[4], line[6]))
+            else:
+                output.append((new_datetime, line[2], line[4]))
+    return output
 
 
 def main():
     log_data = read_log_file(argv[1])
-    print(parse_log_start_time(log_data).tzinfo)
+    for x in parse_frags(log_data):
+        print(x)
 
 
 if __name__ == "__main__":
-    main()    
+    main()
