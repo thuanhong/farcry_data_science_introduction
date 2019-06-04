da1d695e2d7f5f72ec4f7f144461322d2590b5f9
HongThuan <guest@localhost.localdomain>
2019-06-04 13:35:17 +0700

    add waypoint 45

--- sql_query.sql
+++ sql_query.sql
@@ -85,22 +85,18 @@ INNER join match_frag on match.match_id = match_frag.match_id
 group by match.match_id;
 
 #waypoint 44
-/* SQL 01 */
-select match_id, killer_name as player_name, count(killer_name) as kill_count, count(case when victim_name is null then 1 else null end) as suicide_count, killer_name=0 as death_name
-from match_frag
-group by match_id, player_name
-/* SQL 02 */
-select match_id, victim_name as player_name, killer_name=0 as kill_count, victim_name=0 as suicide_count, count(victim_name) as death_name
-from match_frag
-where victim_name is not null
-group by match_id, player_name
-/* SQL 03 */
-select match_id, killer_name as player_name, count(killer_name) as kill_count, count(case when victim_name is null then 1 else null end) as suicide_count, killer_name=0 as death_name
-from match_frag
-group by match_id, player_name
-union all
-select match_id, victim_name as player_name, killer_name=0 as kill_count, victim_name=0 as suicide_count, count(victim_name) as death_name
-from match_frag
-where victim_name is not null
-group by match_id, player_name
-order by match_id, player_name
+select match_id, player_name, kill_count, suicide_count, death_count,round(kill_count *100.0 / (kill_count + suicide_count + death_count), 2) as efficiency
+from   (select match_id, player_name, sum(kill_count) as kill_count, sum(suicide_count) as suicide_count, sum(death_count) as death_count
+		from (select match_id, killer_name as player_name, count(killer_name) as kill_count, count(case when victim_name is null then 1 else null end) as suicide_count, killer_name=0 as death_count
+			  from match_frag
+			  group by match_id, player_name
+			  union all
+			  select match_id, victim_name as player_name, killer_name=0 as kill_count, victim_name=0 as suicide_count, count(victim_name) as death_count
+			  from match_frag
+			  where victim_name is not null
+			  group by match_id, player_name
+			  order by match_id, player_name)
+		group by match_id, player_name)
+order by match_id ASC, player_name desc;
+
+waypoint 45
